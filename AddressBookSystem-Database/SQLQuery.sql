270d61fe4f2065bb6d26302222a5638d6d82c29e
rinkuberde <rinkuberde@gmail.com>
2023-02-14 14:17:47 +0530

    [Rinku] -insert values in table

--- AddressBookSystem-Database/SQLQuery.sql
+++ AddressBookSystem-Database/SQLQuery.sql
@@ -13,3 +13,13 @@ zip int,
 phoneNumber bigint,
 email varchar(200) not null
 )
+
+------ UC 3: Insert Values to Table ------
+insert into  AddressBookDetails
+values('rinku', 'berde', '54 backstreet', 'NY', 'America', 123654, 09876557843, 'rinku@email.com'),
+('gitesh', 'kumar', '12 SM nagar', 'Mysore', 'Karnataka', 100004, 09876554322, 'gitesh@email.com'),
+('terisa', 'sharma', '3 AB nagar', 'Noida', 'UP', 543201, 09876552321, 'terisa@email.com'),
+('harsha', 'yadav', '84 MJ nagar', 'Chennai', 'Tamil Nadu', 129123, 09876554367, 'harsha@email.com')
+
+--display table
+select * from AddressBookDetails
